3a91f754ea75b32df14dadcb328bd3e911af8287
Sherlockh99 <sherlockh989@gmail.com>
2022-01-12 22:26:30 +0200

    HW 10

---
+++ src/lesson10/TelephoneNumbers.java
@@ -0,0 +1,26 @@
+package lesson10;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TelephoneNumbers {
+    private HashMap<String, ArrayList> contacts = new HashMap<>();
+
+    public void addNumber(String fullName, int telNumber){
+        //ArrayList telNumbers = contacts.getOrDefault(fullName,null);
+        ArrayList telNumbers = contacts.get(fullName);
+        if(telNumbers==null){
+            telNumbers = new ArrayList();
+        }
+        telNumbers.add(telNumber);
+        contacts.put(fullName,telNumbers);
+    }
+
+    public void getNumbers(String fullName){
+        //ArrayList telNumbers = contacts.getOrDefault(fullName,null);
+        ArrayList telNumbers = contacts.get(fullName);
+        if(telNumbers!=null){
+            System.out.println(fullName + ": " + telNumbers);
+        }
+    }
+}
